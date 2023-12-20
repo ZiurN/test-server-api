c0f199d544344155d6dd8e26a1d3237dda9e91d7
Jeferson Ruiz <jefryruiz@gmail.com>
2023-12-20 16:10:30 -0300

    Agregado endpoint para cheques por fondo

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-const projectCheque = {
+const projectChequeForma = {
     cic: 1,
     origen: 1,
     estado: 1,
@@ -19,5 +19,28 @@ const projectCheque = {
     saldoDisponibleTotal: 1,
     retenciones: 1
 }
+const projectChequeFondo = {
+    estado: 1,
+    descripcionEstado: 1,
+    accion: 1,
+    criterioAutomatico: 1,
+    motivo: 1,
+    oficina: 1,
+    cheque: {
+        serial: 1,
+        monto: 1,
+        fecha: 1,
+        origenAccion: 1
+    },
+    cuenta: {
+        numero: 1
+    },
+    tipoMovimiento: 1,
+    saldoContable: 1,
+    saldoDisponible: 1,
+    saldoDisponibleCalculado: 1,
+    saldoDisponibleTotal: 1,
+    retenciones: 1
+}
 
-export { projectCheque }
+export { projectChequeForma, projectChequeFondo }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { Database } from './database.js'
-import { projectCheque } from './projects.js'
+import { projectChequeForma, projectChequeFondo } from './projects.js'
 
 const db = new Database('bci1001')
 const app = express()
@@ -22,7 +22,20 @@ app.get('/', (req, res) => {
  * POST urls
  */
 app.post('/protestos-forma-busquedas/por-ejecutivo-fecha', (req, res) => {
-  db.findData('bci_ChequeProtestadoForma', { _userAlias: userAlias}, projectCheque)
+  db.findData('bci_ChequeProtestadoForma', { _userAlias: userAlias}, projectChequeForma)
+    .then(cheques => {
+      if (cheques.length === 0) {
+        res.status(404).send('No se encontraron cheques protestados para el ejecutivo ' + userAlias)
+        return
+      }
+      res.send(cheques)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
+app.post('/protestos-fondo-busquedas/por-ejecutivo-fecha', (req, res) => {
+  db.findData('bci_ChequeProtestadoFondo', { _userAlias: userAlias}, projectChequeFondo)
     .then(cheques => {
       if (cheques.length === 0) {
         res.status(404).send('No se encontraron cheques protestados para el ejecutivo ' + userAlias)
