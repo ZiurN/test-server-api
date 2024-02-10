89cfbbb05d59b87e02b516e56ae1d8701a435e1c
Jeferson Ruiz <jefryruiz@gmail.com>
2024-02-10 15:08:03 -0300

    Correcciones para buscar cuentas, y agregado detalle de cuentas

--- projects.js
+++ projects.js
@@ -84,5 +84,57 @@ const projectDeudaMoraLSG = {
   tasa: 1,
   cuenta: 1
 }
+const projectFinancialAccount = {
+  Id: 1,
+  OwnerId: 1,
+  Name: 1,
+  RecordTypeId: 1,
+  'RecordType.Name': 1,
+  'RecordType.DeveloperName': 1,
+  FinServ__FinancialAccountNumber__c: 1,
+  FinServ__FinancialAccountSource__c: 1,
+  FinServ__FinancialAccountType__c: 1,
+  FinServ__OwnerType__c: 1,
+  FinServ__PrimaryOwner__c: 1,
+  'FinServ__PrimaryOwner__r.bci_cli_rut__c': 1,
+  'FinServ__PrimaryOwner__r.Name': 1,
+  FinServ__RecordTypeName__c: 1,
+  FinServ__Status__c: 1,
+  FinServ__Balance__c: 1,
+  bci_BiPersonal__c: 1,
+  bci_ind_tipo_mov__c: 1,
+  bci_ind_var_precio__c: 1,
+  bci_mon_act__c: 1,
+  bci_mon_cuota__c: 1,
+  bci_mon_inv_ini__c: 1,
+  bci_mon_tot_tasa_spread__c: 1,
+  bci_moneda__c: 1,
+  bci_tasa__c: 1,
+  bci_ult_chq_cobrados__c: 1,
+  bci_id_financialaccount__c: 1,
+  bci_aux_detalle_mora__c: 1,
+  bci_cnt_dias_mora__c: 1,
+  bci_flg_pago_transito__c: 1,
+  bci_prod_code__c: 1,
+  bci_fec_primer_venc__c: 1,
+  bci_nom_ejecutivo_comex__c: 1,
+  bci_nom_ejecutivo_leasing__c: 1,
+  bci_num_rentas_act_vs_total__c: 1,
+  bci_cod_cartera_inv__c: 1,
+  bci_cod_cuenta_inv__c: 1,
+  bci_cod_prod_inv__c: 1,
+  bci_cod_regimen_tributario__c: 1,
+  bci_cod_tipo_cartera__c: 1,
+  bci_fec_ven__c: 1,
+  bci_num_cuenta_tdc__c: 1
+}
 
-export { projectChequeForma, projectChequeFondo, projectDeudaMoraCHIP, projectDeudaMoraCC, projectDeudaMoraLeasing, projectDeudaMoraLSG }
+export {
+  projectChequeForma,
+  projectChequeFondo,
+  projectDeudaMoraCHIP,
+  projectDeudaMoraCC,
+  projectDeudaMoraLeasing,
+  projectDeudaMoraLSG,
+  projectFinancialAccount
+}
--- server.js
+++ server.js
@@ -297,10 +297,13 @@ app.post('/lineas-sobregiro-personas-pagos', (req, res) => {
   let tipoPago = 'ABONO'
   /* variables del request */
   let numProducto = req.body.numeroProducto.toString()
+  let regExp = new RegExp(`.*${numProducto}`, 'g')
   let filter = {
     $or: [
       { numeroCuentaCorriente: numProducto },
-      { numeroCuentaSobregiro: numProducto }
+      { numeroCuentaSobregiro: numProducto },
+      { numeroCuentaCorriente: { $regex: regExp } },
+      { numeroCuentaSobregiro: { $regex: regExp } }
     ]
   }
   switch (tipoPago) {
@@ -336,18 +339,40 @@ app.post('/lineas-sobregiro-personas-pagos', (req, res) => {
           res.status(404).send('No se encontraron deudas morosas para el numero de cuenta ' + numProducto)
           return
         }
-        let codigo = 'ABONO'
-        let mensaje = 'Éxito pagando línea de sobregiro'
-        let data = {
-          mensaje: null,
-          ...results[0]
+        let accountFields = {
+          FinServ__FinancialAccountNumber__c: { $regex: regExp }
         }
-        let response = {
-          codigo: codigo,
-          mensaje: mensaje,
-          data: data
-        }
-        res.status(200).send(response)
+        db.findData('bci_FinancialAccount', accountFields, proyects.projectFinancialAccount)
+          .then(accounts => {
+            if (results.length === 0) {
+              res.status(404).send('No se encontraron deudas morosas para el numero de cuenta ' + numProducto)
+              return
+            }
+            let codigo = 'ABONO'
+            let mensaje = 'Éxito pagando línea de sobregiro'
+            let data = {
+              mensaje: null,
+              ...results[0],
+              cuenta: {
+                saldoDisponible: accounts[0].FinServ__Balance__c,
+                disponibleLinea: 100000
+              },
+              detalleCuenta: {
+                cuenta: results[0].numeroCuentaCorriente,
+              }
+            }
+            let response = {
+              codigo: codigo,
+              mensaje: mensaje,
+              data: data
+            }
+            res.status(200).send(response)
+
+          })
+          .catch(err => {
+            console.log(err)
+            res.status(500).send(err)
+          })
       })
       .catch(err => {
         console.log(err)
