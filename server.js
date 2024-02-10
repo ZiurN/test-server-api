c8f8a587c1652ee6095dfbd4c08c1331594d32fc
Jeferson Ruiz <jefryruiz@gmail.com>
2024-02-10 16:11:02 -0300

    PAGO EXITOS LSG

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ app.post('/creditos-hipotecarios-pagos/por-numero-operacion', (req, res) => {
 })
 app.post('/lineas-sobregiro-personas-pagos', (req, res) => {
   /* para controlar la respuesta */
-  let tipoPago = 'ABONO'
+  let tipoPago = 'PAGO_EXITO'
   /* variables del request */
   let numProducto = req.body.numeroProducto.toString()
   switch (tipoPago) {
